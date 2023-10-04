fe4b0e810b89762db2340fceb9af6080c3569994
Jake Poe <jacobpoe@gmail.com>
2023-10-04 18:58:43 -0400

    WIP project scaffolding

---
+++ src/components/Ballot.js
@@ -0,0 +1,14 @@
+const Ballot = (props) => {
+  return (
+    <>
+      <h1>Ballot</h1>
+      {/**
+       * This component will render one of two options associated with a question.
+       *
+       * It will display the text and the users who have voted for that option.
+       */}
+    </>
+  );
+};
+
+export default Ballot;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+
+import authedUser from './reducers';
+const store = configureStore({ reducer: authedUser });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
