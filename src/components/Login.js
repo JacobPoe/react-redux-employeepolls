2671761a251c7ff1975b6dfbaab9453db52e8b8c
Jake Poe <jacobpoe@gmail.com>
2023-10-05 19:07:16 -0400

    WIP login feature

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,72 @@
+import { useState } from 'react';
 import { connect } from 'react-redux';
 
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
 const Login = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    const unauthedUser = {
+      username: username,
+      password: password
+    };
+    console.log(unauthedUser);
+
+    /**
+     * TODO: Handle login logic
+     * 1) Check to see if user w/ entered username exists in store
+     * 2) If user exists, check if passwords match
+     * 3) If user doesn't exist, alert error
+     * 4) If user exists, update store andredirect to homepage
+     */
+
+    // TODO: Update store to set logged in user on success
+  };
+
   return (
     <div
       className="modal show"
       style={{ display: 'block', position: 'initial' }}
     >
       <Modal.Dialog>
-        <Modal.Header closeButton>
+        <Modal.Header>
           <Modal.Title>Log In</Modal.Title>
         </Modal.Header>
 
-        <Modal.Body>
-          <p>Please log in to vote</p>
-        </Modal.Body>
+        <Form onSubmit={handleLogin}>
+          <Modal.Body>
+            <p>Please log in to vote</p>
+            <Form.Group className="mb-3" controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </Form.Group>
 
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </Form.Group>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button type="submit" variant="primary">
+              Log In
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal.Dialog>
     </div>
   );
