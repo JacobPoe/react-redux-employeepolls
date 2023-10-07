dda3619b3838f25e5f4e50f04f60e6a5d074a067
Jake Poe <jacobpoe@gmail.com>
2023-10-06 22:09:55 -0400

    Homepage split into two tabs of questions

--- src/components/Ballot.js
+++ src/components/Ballot.js
@@ -3,12 +3,12 @@ import { connect } from 'react-redux';
 const Ballot = (props) => {
   return (
     <>
-      <h1>Ballot</h1>
-      {/**
-       * This component will render one of two options associated with a question.
-       *
-       * It will display the text and the users who have voted for that option.
-       */}
+      <h4>{props.option.text}</h4>
+      <ul>
+        {props.option.votes.map((voter) => {
+          return <li key={voter}>{voter}</li>;
+        })}
+      </ul>
     </>
   );
 };
