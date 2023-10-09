89d7115c7f7aa67881a711a80921e50ddabaf19a
Jake Poe <jacobpoe@gmail.com>
2023-10-08 20:33:34 -0400

    /new endpoint and interactions working

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,8 +1,14 @@
+import { ADD_QUESTION } from '../actions/questions';
 import { CAST_VOTE, RECEIVE_DATA } from '../actions/shared';
 
 export default function questions(state = [], action) {
   console.log(state);
   switch (action.type) {
+    case ADD_QUESTION:
+      return {
+        ...state,
+        [action.question.id]: action.question
+      };
     case CAST_VOTE:
       return {
         ...state,
